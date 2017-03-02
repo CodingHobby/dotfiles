928b24c8df9cc569c9d44f31eac4ccb6b4efaf21
Corrado Canepari <corrado@codinghobby.station>
2017-03-02 12:08:57 +0000

    Changed zsh-setup

--- Shell/zsh-setup.sh
+++ Shell/zsh-setup.sh
@@ -6,9 +6,7 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 # Customizing oh-my-zsh
 figlet "Customizing OH-MY-ZSH..."
 cp /home/corrado/Dotfiles/Shell/.zshrc /home/corrado
-if ! [ -d "/home/corrado/.oh-my-zsh/custom/themes" ]; then
-	mkdir /home/corrado/.oh-my-zsh/custom/themes
-fi
-	cp /home/corrado/Dotfiles/Shell/arrow.zsh-theme /home/corrado/.oh-my-zsh/custom/themes
+mkdir /home/corrado/.oh-my-zsh/custom/themes
+cp /home/corrado/Dotfiles/Shell/arrow.zsh-theme /home/corrado/.oh-my-zsh/custom/themes
 
 source /home/corrado/.zshrc
